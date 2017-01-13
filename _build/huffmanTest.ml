0cc5867dd589c8058d6f3c1e9861e63504034faa
Kennik89 <kand89@gmail.com>
2017-01-13 13:40:17 +0100

    Minor added

--- _build/huffmanTest.ml
+++ _build/huffmanTest.ml
@@ -1,15 +1,14 @@
 open QCheck
 
-
 external huffman_Compress : bytes -> bytes -> int -> int = "encode_stub"
 external huffman_Uncompress : bytes -> bytes -> int -> int -> unit= "decode_stub"
 
-(* if ((Array.length Sys.argv) > 1) then ()
-  let s1 = Bytes.of_string Sys.argv.(1)
+(* if ((Array.length Sys.argv) > 1) then () *)
+  let s1 = Bytes.of_string "Hello"
   let l = String.length s1
-
   let s2 = Bytes.create (l+384)
 
+(*
   (*Printing an integer representing the length of the output*)
   let testCompress = huffman_Compress s1 s2 l;;
   print_int testCompress;;
@@ -41,7 +40,6 @@ let output s = Bytes.create ((len s)+384);;
 
 let compress input = huffman_Compress (orig input) (output input) (len input);;
 
-
 (*  comp : string -> bytes*)
 let comp s =
   let input = Bytes.of_string s in
@@ -57,12 +55,9 @@ let decomp b l =
   let () = huffman_Uncompress b output inputLength l in
   Bytes.to_string output;;
 
-(*Test to test if tests can test (testing if QCheck works)*)
-(*let testTest = Test.make
-        (int)(fun i -> i=i)
-let _= QCheck_runner.run_tests [testTest]*)
+let someTest = Test.make ~count:1000 (*~print:string_of_inputs *)
+  (string_gen_of_size (int_range 0 1000).gen char.gen)
+  (fun s -> let length = String.length s in
+    s = comp( decomp( decomp (comp s) length) length));;
 
-let someTest = Test.make ~count:1000
-            (string_gen_of_size (int_range 0 100000).gen char.gen)(fun s -> let length = String.length s in
-                                s = (decomp (comp s) length));;
 let _= QCheck_runner.run_tests ~verbose:true [someTest]
